e84bf1031a8cf25b83765e5398bdeb034aab6d6a
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-08 18:03:07 +0530

    Modify script to update webpack configs

--- sass.sh
+++ sass.sh
@@ -15,19 +15,30 @@ echo "Adding import for main.scss to main.ts.."
 echo "import '../scss/main.scss'" > src/ts/main.ts
 echo "Updating postcss configuration.."
 rm postcss.config.js
-echo "const purgecss = require('@fullhuman/postcss-purgecss');module.exports = { plugins: [require('autoprefixer'), purgecss({ content: ['./**/*.html'] })], }" > postcss.config.js
+echo "const purgecss = require('@fullhuman/postcss-purgecss');module.exports = { plugins: [require('autoprefixer'), purgecss({ content: ['./**/*.html', './src/**/*.ts'] })], }" > postcss.config.js
 echo "Updating prettier configuration.."
 sed '5d' .prettierrc > temp
 rm .prettierrc
 cat temp > .prettierrc && rm temp
 echo "Updating webpack configuration.."
-sed "s/postcss-loader/postcss-loader', 'sass-loader/" webpack.common.js > temp
+sed '34,38d' webpack.common.js > temp
 rm webpack.common.js
 cat temp > webpack.common.js && rm temp
+sed '10 amodule: { rules: [{test: /\.s?css$/i, include: path.resolve(__dirname, "src"),use: ["style-loader", "css-loader", "sass-loader"],},],}' webpack.dev.js > temp
+rm webpack.dev.js
+cat temp > webpack.dev.js && rm temp
+sed '6 amodule: {rules: [{test: /\.s?css$/i, include: path.resolve(__dirname, "src"), use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],},],}' webpack.prod.js > temp
+rm webpack.prod.js
+cat temp > webpack.prod.js && rm temp
+sed '1 iconst path = require("path");' webpack.prod.js > temp
+rm webpack.prod.js
+cat temp > webpack.prod.js && rm temp
 echo "Finishing up.."
 npx prettier .prettierrc --write
 npx prettier postcss.config.js --write
 npx prettier webpack.common.js --write
+npx prettier webpack.dev.js --write
+npx prettier webpack.prod.js --write
 clear
 echo "Project setup successful!"
 rm sass.sh
